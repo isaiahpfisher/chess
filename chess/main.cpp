9574a8abc018b0fc23407c6aedf1904ca8cad065
isaiahpfisher <isaiahpfisher@gmail.com>
2023-11-09 14:43:33 -0600

    More testing!

--- chess/main.cpp
+++ chess/main.cpp
@@ -1,6 +1,12 @@
 #include "header.h"
 
 int main() {
+
+    vector<int> nums = { 2, 9, 29, 72 };
+
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << endl;
+    }
     
     cout << "WASSUP!";
 
