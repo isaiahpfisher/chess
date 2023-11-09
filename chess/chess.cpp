4669d83f96046e40c9d16063f15d45739c47efd4
isaiahpfisher <isaiahpfisher@gmail.com>
2023-11-09 14:31:07 -0600

    More revert testing

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -1,12 +1,7 @@
 #include "header.h"
 
 int main() {
-    vector<int> myFavoriteNumbers = { 1, 2, 3, 4, 5 };
-    for (int i = 0; i < myFavoriteNumbers.size(); i++) {
-        cout << myFavoriteNumbers[i] << endl;
-    }
     
-    cout << "This is a test" << endl;
 
     return 0;
 }
