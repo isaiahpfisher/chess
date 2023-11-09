f59293c16541fe06d73bd26534a1016eaf54284c
Jaxen Roden <jaxer0920@gmail.com>
2023-11-09 13:11:11 -0600

    Test

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main() {
@@ -7,4 +8,8 @@ int main() {
     for (int i = 0; i < myFavoriteNumbers.size(); i++) {
         cout << myFavoriteNumbers[i] << endl;
     }
+
+    cout << "This is a test" << endl;
+
+    return 0;
 }
