0f4b42fbdf5968538908e57ffa53ff4000c07089
isaiahpfisher <isaiahpfisher@gmail.com>
2023-11-09 13:10:15 -0600

    Total Program Overhaul

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    cout << "Hello World!";
+    vector<int> myFavoriteNumbers = { 1, 2, 3, 4, 5 };
+    for (int i = 0; i < myFavoriteNumbers.size(); i++) {
+        cout << myFavoriteNumbers[i] << endl;
+    }
 }
