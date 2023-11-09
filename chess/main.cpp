15f72ab4889dc40277354bdcdcb39db8038e7c8c
isaiahpfisher <isaiahpfisher@gmail.com>
2023-11-09 14:09:24 -0600

    Revert "Deleted main"
    This reverts commit 29fdc4fd002b0b53ee1237a38ccdeb86fc410e22.

--- chess/main.cpp
+++ chess/main.cpp
@@ -1,7 +1,12 @@
 #include "header.h"
 
 int main() {
-    
+    vector<int> myFavoriteNumbers = { 1, 2, 3, 4, 5 };
+    for (int i = 0; i < myFavoriteNumbers.size(); i++) {
+        cout << myFavoriteNumbers[i] << endl;
+    }
+
+    cout << "This is a test" << endl;
 
     return 0;
 }
