46292e409110aa2eef579577603f032f717c9a84
Jaxen Roden <jaxer0920@gmail.com>
2023-11-11 09:57:48 -0600

    Real isPieceInWay

--- chess/Piece.cpp
+++ chess/Piece.cpp
@@ -45,12 +45,39 @@ bool Piece::isPieceInWay(Piece* grid[8][8], int startRow, int startCol, int endR
 	int maxCol = (startCol > endCol ? startCol : endCol);
 	int minCol = (startCol < endCol ? startCol : endCol);
 
-	// loop through grid and check every piece between start and end
-	for (int row = 0; row < 8; row++) {
-		for (int col = 0; col < 8; col++) {
-			if ((((row > minRow && row < maxRow) || (minRow == maxRow)) && ((col > minCol && col < maxCol) || (minCol == maxCol))) && (abs(row - startRow) == abs(col - startCol))) {
-				if (!grid[row][col]->isEmpty()) {
-					checkResult = true;
+
+	// moving front to back
+	if (minRow != maxRow && minCol == maxCol) {
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				if (col == minCol && row > minRow && row < maxRow) {
+					if (!grid[row][col]->isEmpty()) {
+						checkResult = true;
+					}
+				}
+			}
+		}
+	}
+	// side to side
+	else if (minRow == maxRow && minCol != maxCol) {
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				if (row == minRow && col > minCol && col < maxCol) {
+					if (!grid[row][col]->isEmpty()) {
+						checkResult = true;
+					}
+				}
+			}
+		}
+	}
+	// diagonal moves
+	else {
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				if (((row > minRow && row < maxRow) && (col > minCol && col < maxCol)) && (abs(row - startRow) == abs(col - startCol))) {
+					if (!grid[row][col]->isEmpty()) {
+						checkResult = true;
+					}
 				}
 			}
 		}
