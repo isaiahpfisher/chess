072ddd39c3c20c8f1454688c3cd481b8d89da9dc
Jaxen Roden <jaxer0920@gmail.com>
2023-11-11 08:32:15 -0600

    rook moves updated

--- chess/Rook.cpp
+++ chess/Rook.cpp
@@ -12,6 +12,12 @@ string Rook::isValidMove(Piece* grid[8][8], int startRow, int startCol, int endR
 	if (startCol != endCol && startRow != endRow) {
 		checkResult = "Rook can only move forward, left, or right. Try Again.";
 	}
+	else if (this->color == WHITE && !(grid[startRow - 1][startCol]->isEmpty())) {
+		checkResult = "Rook can't move if something is next to it or in front of it. Try Again.";
+	}
+	else if (this->color == BLACK && !(grid[startRow + 1][startCol]->isEmpty())) {
+		checkResult = "Rook can't move if something is next to it or in front of it. Try Again.";
+	}
 
 	return checkResult;
 }
